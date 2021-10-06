5a902f158e1df0f3bd70bd9a899c49a679c9ecca
clide7029 <70732345+clide7029@users.noreply.github.com>
2021-10-05 21:03:34 -0700

    added comments and polished overall functions / game logic

--- app.js
+++ app.js
@@ -4,9 +4,25 @@ var sub_words = new Array();
 var guessed = new Map();
 
 
+// trim down the dictionary
+setUpGame();
+
+// start the game immediately when the page loads
 document.addEventListener('DOMContentLoaded', startGame());
 
-function getRootWords() {
+
+/* 
+initialize one-time setup of important data structures
+*/
+function setUpGame() {
+
+    // eliminate all words less than 3 or greater than 6 letters long
+    for (i = 0; i < dictionary.length; i++) {
+        if (dictionary[i].length <= 6 && dictionary[i].length >= 3) {
+            dict.push(dictionary[i]);
+        }
+    }
+
     // find possible root words (contain 6 letters)
     for (i = 0; i < dict.length; i++) {
         if (dict[i].length == 6) {
@@ -15,13 +31,18 @@ function getRootWords() {
     }
 }
 
+/* 
+checks whether a candidate word is a subword the first word 
+@param word the root word
+@param subword the candidate subword
+ */
 function isSubWord(word, subword) {
     let w = word;
-    let seen = [];
+    // check each character in candidate sub-word for membership in root-word
     for (let i = 0; i < subword.length; i++) {
         if (w.includes(subword[i])) {
+            // remove char from temp word (checking for char duplicates)
             w = w.replace(subword[i], '');
-            seen.push(subword[i]);
         } else {
             return false;
         }
@@ -29,12 +50,20 @@ function isSubWord(word, subword) {
     return true;
 }
 
+/* 
+takes a dictionary and a word, creating a sub-array of sub-words
+@param word the root-word
+@param dict the dictionary of candidate words
+@return subwords a sub-array of dict which contains only sub-words of word
+ */
 function trimDict(word, dict) {
     let i = 0;
     subwords = dict;
+    // until there are no valid word left to check:
     while (subwords[i] !== null && typeof subwords[i] !== 'undefined') {
+        // see if that word is a sub-word of target root-word
         if (!isSubWord(word, subwords[i])) {
-            // console.log("removed [ " + subwords[i] + " ]");
+            // if not, get rid of it
             subwords.splice(i, 1);
         } else {
             i++;
@@ -43,27 +72,58 @@ function trimDict(word, dict) {
     return subwords;
 }
 
+/* 
+scrambles the order of letters in a String
+@param word the word to mix up
+@return STRING the scrambled word
+ */
 function scramble(word) {
+    // turn string into array
     arr = word.split('');
     for (let i = 0; i < arr.length - 1; i++) {
+        // choose a random idx
         idx = Math.floor(Math.random() * (word.length));
 
+        // swap char
         char = arr[i];
         arr[i] = arr[idx];
         arr[idx] = char;
         word.replace(char, word[idx]);
     }
 
+    // turn array back into string and return it
     return arr.join('');
 }
 
+/* 
+returns a random word and sets sub_words to be only 
+the sub-words of chosen random word
+@return word the random word
+ */
+function getRandomWord() {
+    // get random word
+    r = Math.floor(Math.random() * (root_words.length - 1));
+    word = root_words[r];
+
+    // create array of subwords for chosen random word
+    sub_words = trimDict(word, dict);
+
+    return word;
+}
+
+/* 
+print the current state of the game
+@param word the scrambled word to be displayed to user
+ */
 function printGame(word) {
     console.clear();
     console.log("Available letters: " + word);
     for (let i = 0; i < sub_words.length; i++) {
         if (guessed.get(sub_words[i])) {
+            // reveal guessed words
             console.log(sub_words[i]);
         } else {
+            // hide unguessed words
             blank = "";
             for (let j = 0; j < sub_words[i].length; j++) {
                 blank += '-';
@@ -74,50 +134,62 @@ function printGame(word) {
     }
 }
 
+/* 
+end the game and prompt user to replay 
+@param word the goal word to be displayed to user 
+@return BOOLEAN whether the user wants to replay
+ */
 function endGame(word) {
     console.clear();
     console.log("the word was: " + word);
+
+    // reveal all the sub-words
     for (let i = 0; i < sub_words.length; i++) {
         console.log(sub_words[i]);
     }
+
     console.log("Congratulation, you guessed " + guessed.size + " / " + sub_words.length + " words correctly")
 
+    // clear all global data structures
+    dict.splice(0, dict.length);
+    root_words.splice(0, root_words.length);
     sub_words.splice(0, sub_words.length);
     guessed.clear();
 
+    // prompt user to replay
     return confirm("want to try again?");
 }
 
+/* 
+start the game and select a random word
+ */
 function startGame() {
 
-    for (i = 0; i < dictionary.length; i++) {
-        if (dictionary[i].length <= 6 && dictionary[i].length >= 3) {
-            dict.push(dictionary[i]);
-        }
-    }
+    // pick a random target word
+    word = getRandomWord();
 
-    getRootWords();
-
-    let r = Math.floor(Math.random() * (root_words.length - 1));
-    // let word = "bat";
-    let word = root_words[r];
-    sub_words = trimDict(word, dict);
-
+    // get the game going
     if (gameLoop(word)) {
         location.reload(true);
-        startGame();
     }
 
 }
 
-
+/* 
+repeatedly prompt user to input guesses
+@param word the target root-word
+@return BOOLEAN whether the user wants to replay
+ */
 function gameLoop(word) {
 
     scram = scramble(word);
     printGame(scram);
 
     while (true) {
+        // prompt the user for input
         guess = prompt("enter a guess:");
+
+        // if they quit or hit cancel, exit
         if (guess === null || guess === "quit") {
             return endGame(word);
         }
@@ -125,18 +197,32 @@ function gameLoop(word) {
         // user wants to scramble word
         if (guess === "*") {
             scram = scramble(scram);
+            printGame(scram);
             alert("available letters have been scrambled!");
+            continue;
         }
 
-        // update list of guessed
-        if (dict.includes(guess)) {
+        /* evaluate user's guess */
+        if (guessed.get(guess)) {
+            // guess already found
+            alert("word has already been found");
+
+        } else if (dict.includes(guess)) {
+            // guessed correctly
             guessed.set(guess, true);
-            alert("you guessed: " + guess);
+            alert("you correctly guessed: " + guess);
+
+        } else {
+            // guessed is not correct   
+            alert("word guessed is not a valid English sub-word (or too long/short)")
         }
 
+        // user guessed all the sub-words. They Win!
         if (guessed.size === sub_words.length) {
-            endGame(word);
+            return endGame(word);
         }
+
+        // print current state of the game
         printGame(scram);
     }
 
