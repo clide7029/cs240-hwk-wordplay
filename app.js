e33ef06c7ab46737fc6afd632268fbf871029ae5
clide7029 <70732345+clide7029@users.noreply.github.com>
2021-10-05 19:08:25 -0700

    fixed permuatation / sub-word function

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var dict = new Array();
 var root_words = new Array();
-var child_map = new Map();
-var word_exists = new Map();
-var checked = new Array();
 var sub_words = new Array();
-// var sub_set = new Set();
+var guessed = new Map();
 
 
 function getRootWords() {
@@ -12,90 +9,38 @@ function getRootWords() {
     for (i = 0; i < dict.length; i++) {
         if (dict[i].length == 6) {
             root_words.push(dict[i]);
-            child_map.set(dict[i], subwords = new Set());
-            word_exists.set(dict[i], true);
         }
     }
 }
 
-
-
-
-// function getSubWords(word, sub_set) {
-//     let subword;
-//     for (i = 0; i < word.length; i++) {
-//         subword = "";
-//         if (i == 0) {
-//             subword = word.slice(i + 1, word.length);
-
-//         } else if (i == word.length - 1) {
-//             subword = word.slice(0, i - 1);
-//             console.log("here");
-
-//         } else {
-//             subword = subword.concat(word.splice(0, i - 1));
-
-//             console.log("string" + subword);
-
-//             subword = subword.concat(word.splice(i + 1, word.length - 1));
-//         }
-
-//         console.log(subword);
-
-//         if (word_exists.has(subword)) {
-//             sub_set.add(subword);
-
-//         } else {
-//             console.log("not a valid word");
-//         }
-//         getSubWords(subword, sub_set);
-//     }
-//     // return 
-// }
-
-function arrayify(string) {
-    const array = [];
-    for (i = 0; i < string.length; i++) {
-        array.push(string[i]);
+function isSubWord(word, subword) {
+    let w = word;
+    let seen = [];
+    for (let i = 0; i < subword.length; i++) {
+        if (w.includes(subword[i])) {
+            w = w.replace(subword[i], '');
+            seen.push(subword[i]);
+        } else {
+            return false;
+        }
     }
-    return array;
+    return true;
 }
 
-function removeNonCandidateWords(word, dictionary) {
-    let dict = dictionary;
-    j = 0;
-    for (i = 0; i < dict.length; i++) {
-        //     word_arr = arrayify(dict[i]);
-        //     word_arr.map(char => {
-        //         if (!(word.includes(char))) {
-        //             console.log("removed");
-        //             dict.splice(i, 1);
-        //             j = 0;
-        //         }
-        //         j++;
-        //     })
-        if (i >= dict.length) {
-            console.log("ERRRRORR");
-            break;
+function trimDict(word, dict) {
+    let i = 0;
+    subwords = dict;
+    while (subwords[i] !== null && typeof subwords[i] !== 'undefined') {
+        if (!isSubWord(word, subwords[i])) {
+            // console.log("removed [ " + subwords[i] + " ]");
+            subwords.splice(i, 1);
+        } else {
+            i++;
         }
-
-        while (typeof dict[i] !== 'undefined') {
-            // console.log(dict[i].slice(j, j + 1));
-            // console.log(dict[i]);
-            //  || j >= word.length
-            let wordCandidate = dict[i];
-            if (!(word.includes(wordCandidate[j]))) {
-                sub = dict.splice(i, 1);
-                console.log("removed [ " + sub + " ]");
-                j = 0;
-            }
-            j++;
-        }
-        console.log("j = " + j);
-        console.log(dict[i]);
     }
-    return dict;
+    return subwords;
 }
+
 
 
 
@@ -109,34 +54,33 @@ function startGame() {
 
     getRootWords();
 
-    var word = "logger";
-    var subword = "gore";
-    var sub = "george";
+    let r = Math.floor(Math.random() * (root_words.length - 1));
+    let word = root_words[r];
+    sub_words = trimDict(word, dict);
 
-
-    // var sub_set = child_map.get(word);
-
-
-    getSubWords(word, sub_set);
-
-    dict = removeNonCandidateWords(word, dict);
-
-
-
-
-
-    console.log(dict);
+    gameLoop(word);
 
 }
 
-function gameLoop() {
 
-    while ((guess = prompt("enter a guess:")) !== "quit") {
-        if (guess === null) {
+function gameLoop(word) {
+
+
+    while (true) {
+        guess = prompt("enter a guess:");
+        if (guess === null || guess === "quit") {
             console.log("canceled");
             break;
         }
-        console.log(guess);
+
+        if (guess === "*") {
+            scram = scramble(scram);
+            alert("available letters have been scrambled!");
+        }
+        if (dict.includes(guess)) {
+            guessed.set(guess, true);
+            alert("you guessed: " + guess);
+        }
 
     }
 
